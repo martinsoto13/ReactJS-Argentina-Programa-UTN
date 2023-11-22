6ae5ec25781ec9fa89cd751569ad2d46f455699d
martinsoto13 <martin.maldonado26@gmail.com>
2023-11-21 23:46:04 -0300

    footer

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
 
 function TaskItem({ task, completeTask, deleteTask }) {
+  const [isAlertOpen, setAlertOpen] = useState(false);
+
   const handleComplete = () => {
     completeTask();
   };
 
   const handleDelete = () => {
+    // Mostrar un alert para confirmar la eliminación
+    setAlertOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    // Si el usuario confirma, ejecutar la función de eliminación
     deleteTask();
+    setAlertOpen(false);
+  };
+
+  const handleCancelDelete = () => {
+    // Si el usuario cancela, cerrar el alert
+    setAlertOpen(false);
   };
 
   return (
@@ -14,9 +28,18 @@ function TaskItem({ task, completeTask, deleteTask }) {
       <p>{task.name}</p>
       <button onClick={handleComplete}>Completar</button>
       <button onClick={handleDelete}>Eliminar</button>
+
+      {isAlertOpen && (
+        <div className="delete-alert">
+          <p>¿Estás seguro de que quieres eliminar esta tarea?</p>
+          <button onClick={handleConfirmDelete}>Sí</button>
+          <button onClick={handleCancelDelete}>No</button>
+        </div>
+      )}
     </div>
   );
 }
 
 export default TaskItem;
+
 
